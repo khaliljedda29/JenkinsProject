1b203f268b8cc31b1ac8341d2903e425424c9911
youssefskhiri <youssef.skhiri@esprit.tn>
2022-10-15 01:48:00 +0100

    Test 1 after sonar

---
+++ src/main/java/com/esprit/examen/controllers/ProduitRestController.java
@@ -0,0 +1,102 @@
+package com.esprit.examen.controllers;
+
+
+import java.util.List;
+
+import com.esprit.examen.entities.ProduitDto;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.esprit.examen.entities.Produit;
+import com.esprit.examen.services.IProduitService;
+
+import io.swagger.annotations.Api;
+
+
+@RestController
+@CrossOrigin("*")
+@Api(tags = "Gestion des produits")
+@RequestMapping("/produit")
+public class ProduitRestController {
+
+	@Autowired
+	IProduitService produitService;
+	@Autowired
+	private ModelMapper modelMapper;
+
+	// http://localhost:8089/SpringMVC/produit/retrieve-all-produits
+	@GetMapping("/retrieve-all-produits")
+	@ResponseBody
+	public List<Produit> getProduits() {
+		return produitService.retrieveAllProduits();
+	}
+
+	// http://localhost:8089/SpringMVC/produit/retrieve-produit/8
+	@GetMapping("/retrieve-produit/{produit-id}")
+	@ResponseBody
+	public Produit retrieveRayon(@PathVariable("produit-id") Long produitId) {
+		return produitService.retrieveProduit(produitId);
+	}
+
+	/* Ajouter en produit tout en lui affectant la catégorie produit et le stock associés */
+	// http://localhost:8089/SpringMVC/produit/add-produit/{idCategorieProduit}/{idStock}
+	@PostMapping("/add-produit")
+	@ResponseBody
+	public ResponseEntity<ProduitDto> addProduit(@RequestBody ProduitDto produitDto) {
+		Produit produitRequest = modelMapper.map(produitDto, Produit.class);
+
+		Produit post = produitService.addProduit(produitRequest);
+
+		// convert entity to DTO
+		ProduitDto produitResponse = modelMapper.map(post, ProduitDto.class);
+
+		return new ResponseEntity<>(produitResponse, HttpStatus.CREATED);
+	}
+
+	//http://localhost:8089/SpringMVC/produit/remove-produit/{produit-id}
+	@DeleteMapping("/remove-produit/{produit-id}")
+	@ResponseBody
+	public void removeProduit(@PathVariable("produit-id") Long produitId) {
+		produitService.deleteProduit(produitId);
+	}
+
+	// http://localhost:8089/SpringMVC/produit/modify-produit/{idCategorieProduit}/{idStock}
+	@PutMapping("/modify-produit")
+	@ResponseBody
+	public ResponseEntity<ProduitDto> updateProduit(@RequestBody ProduitDto produitDto) {
+		Produit produitRequest = modelMapper.map(produitDto, Produit.class);
+
+		Produit produit = produitService.updateProduit(produitRequest);
+
+		// entity to DTO
+		ProduitDto produitResponse = modelMapper.map(produit, ProduitDto.class);
+
+		return ResponseEntity.ok().body(produitResponse);
+	}
+
+	/*
+	 * Si le responsable magasin souhaite modifier le stock du produit il peut
+	 * le faire en l'affectant au stock en question
+	 */
+	// http://localhost:8089/SpringMVC/produit/assignProduitToStock/1/5
+	@PutMapping(value = "/assignProduitToStock/{idProduit}/{idStock}")
+	public void assignProduitToStock(@PathVariable("idProduit") Long idProduit, @PathVariable("idStock") Long idStock) {
+		produitService.assignProduitToStock(idProduit, idStock);
+	}
+
+	/*
+	 * Revenu Brut d'un produit (qte * prix unitaire de toutes les lignes du
+	 * detailFacture du produit envoyé en paramètre )
+	 */
+	// http://localhost:8089/SpringMVC/produit/getRevenuBrutProduit/1/{startDate}/{endDate}
+/*	@GetMapping(value = "/getRevenuBrutProduit/{idProduit}/{startDate}/{endDate}")
+	public float getRevenuBrutProduit(@PathVariable("idProduit") Long idProduit,
+			@PathVariable(name = "startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date startDate,
+			@PathVariable(name = "endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date endDate) {
+
+		return produitService.getRevenuBrutProduit(idProduit, startDate, endDate);
+	}*/
+
+}
