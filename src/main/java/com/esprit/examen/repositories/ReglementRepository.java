f9d55110875217d2d8710528b24846641773c417
unknown <arwa.bejaoui@esprit.tn>
2022-10-16 18:14:51 +0200

    commit junit2

--- src/main/java/com/esprit/examen/repositories/ReglementRepository.java
+++ src/main/java/com/esprit/examen/repositories/ReglementRepository.java
@@ -16,7 +16,8 @@ import com.esprit.examen.entities.Reglement;
 public interface ReglementRepository extends CrudRepository<Reglement, Long>{
 	@Query("SELECT r FROM Reglement r where r.facture.idFacture=:idFacture")
 	List<Reglement> retrieveReglementByFacture(@Param("idFacture") Long idFacture);
-
+	@Query("SELECT r FROM Reglement r ")
+	List<Reglement> retrieveAllReglements();
 	
 	@Query("SELECT sum(r.montantPaye) FROM Reglement r where  r.dateReglement between :startDate"
 			+ " and :endDate and r.facture.archivee=false")
--- src/test/java/com/esprit/examen/services/ReglementServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/ReglementServiceImplTest.java
@@ -1,2 +1,54 @@
-package com.esprit.examen.services;public class ReglementServiceImplTest {
+
+
+package com.esprit.examen.services;
+
+import static org.junit.Assert.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.esprit.examen.entities.Reglement;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import com.esprit.examen.repositories.ReglementRepository;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class ReglementServiceImplTest {
+    @Autowired
+    IReglementService rs;
+
+    @Autowired
+    ReglementRepository rr;
+
+    @Test
+    @Order(1)
+    public void testRetrieveAllReglement()
+    {
+        List<Reglement> listReglements = rs.retrieveAllReglements();
+        Assertions.assertEquals(10, listReglements.size());
+    }
+
+
+
 }
+
