383d90d340651ecd6a2217b0d81d5303904bb9d6
youssefskhiri <youssef.skhiri@esprit.tn>
2022-10-17 03:15:35 +0100

    Test 38 JUnit

--- src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -1,4 +1,4 @@
-/*package com.esprit.examen.services;
+package com.esprit.examen.services;
 
 import static org.junit.Assert.*;
 import java.util.List;
@@ -24,7 +24,7 @@ public class StockServiceImplTest {
 		
 	//	assertEquals(expected+1, stockService.retrieveAllStocks().size());
 		assertNotNull(savedStock.getLibelleStock());
-		stockService.deleteStock(savedStock.getIdStock());
+		//stockService.deleteStock(savedStock.getIdStock());
 		
 	} 
 	
@@ -48,4 +48,4 @@ public class StockServiceImplTest {
 		assertNull(stockService.retrieveStock(savedStock.getIdStock()));
 	}
 
-}*/
+}
